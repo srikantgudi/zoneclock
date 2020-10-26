7c37c7c259fe1e943244ccac4f81fdd2b8d1dfd3
srikantgudi <53587620+srikantgudi@users.noreply.github.com>
2020-10-26 14:27:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ option {
         <circle class='clock-face' r='190' cx='200' cy='200' />
         <circle r='140' cx='200' cy='200' fill='#cde' />
         <polyline id='thr' fill='blue' points='200,200 210,210 350,200 210,190' />
-        <polyline id='tmi' fill='navy' points='200,200 210,215 380,200 210,195' />
-        <polyline id='tse' fill='olive' points='200,200 210,212 380,200 210,198' />
+        <polyline id='tmi' fill='navy' points='200,200 210,210 380,200 210,190' />
+        <polyline id='tse' fill='olive' points='200,200 210,208 380,200 210,195' />
       </svg>
     </div>
   </div>
